b0103511abceff42a2e9a3f9a172f72276f2c68e
Hamlet <hamlet.villa@gmail.com>
2019-04-16 02:08:26 +0000

    second db query added

--- log_report.py
+++ log_report.py
@@ -6,13 +6,68 @@
 
 import psycopg2 as psy
 
-db_query1 = '''select articles.title, count(log.method) as views
-from articles, log where '/article/' || articles.slug = log.path
-group by articles.title order by views desc limit 3;'''
 
-db = psy.connect("dbname=news")
-db_cursor = db.cursor()
-db_cursor.execute()
-data = db_cursor.fetchall()
-db.close()
-print data
+def getTopThreeArticles():
+
+    db_query1 = '''
+    SELECT articles.title, COUNT(log.method) AS views
+    FROM articles, log WHERE '/article/' || articles.slug = log.path
+    GROUP BY articles.title
+    ORDER BY views DESC LIMIT 3;
+    '''
+
+    db = psy.connect(database = "news")
+    db_cursor = db.cursor()
+    db_cursor.execute(db_query1)
+    query_result = db_cursor.fetchall()
+    for result in query_result:
+        print(result[0] + " - " + str(result[1]) + " views")
+
+    db_cursor.close()
+    db.close()
+
+def getPopularAuthors():
+
+    db_query2 = '''
+    SELECT authors.name, COUNT(log.status) AS views
+    FROM authors JOIN articles ON authors.id = articles.author
+    JOIN log ON '/article/' || articles.slug = log.path
+    AND log.status = '200 OK'
+    GROUP BY authors.name
+    ORDER BY views DESC;
+    '''
+
+    db = psy.connect(database = "news")
+    db_cursor = db.cursor()
+    db_cursor.execute(db_query2)
+    query_result = db_cursor.fetchall()
+    for result in query_result:
+        print(result[0] + " - " + str(result[1]) + " views")
+
+    db_cursor.close()
+    db.close()
+
+def getDaysErrors():
+
+    db_query3 = '''
+    SELECT authors.name, COUNT(log.status = '200 OK') AS views
+    FROM authors JOIN articles ON authors.id = articles.author
+    JOIN log ON '/article/' || articles.slug = log.path
+    GROUP BY authors.name
+    ORDER BY views DESC;
+    '''
+
+    db = psy.connect(database = "news")
+    db_cursor = db.cursor()
+    db_cursor.execute(db_query3)
+    query_result = db_cursor.fetchall()
+    for result in query_result:
+        print(result[0] + " - " + str(result[1]) + " views")
+
+    db_cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    getTopThreeArticles()
+    getPopularAuthors()
+    getDaysErrors()
