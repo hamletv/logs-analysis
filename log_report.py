4724441c7264f2c141860f9a33a84d19732a62b2
Hamlet <hamlet.villa@gmail.com>
2019-04-16 02:22:44 +0000

    connect db function to keep DRY

--- log_report.py
+++ log_report.py
@@ -6,29 +6,32 @@
 
 import psycopg2 as psy
 
+def connectAccessDB(db_query):
+
+    db = psy.connect(database = "news")
+    db_cursor = db.cursor()
+    db_cursor.execute(db_query)
+    query_result = db_cursor.fetchall()
+    db_cursor.close()
+    db.close()
+    return query_result
 
 def getTopThreeArticles():
 
-    db_query1 = '''
+    db_query = '''
     SELECT articles.title, COUNT(log.method) AS views
     FROM articles, log WHERE '/article/' || articles.slug = log.path
     GROUP BY articles.title
     ORDER BY views DESC LIMIT 3;
     '''
 
-    db = psy.connect(database = "news")
-    db_cursor = db.cursor()
-    db_cursor.execute(db_query1)
-    query_result = db_cursor.fetchall()
-    for result in query_result:
+    db_result = connectAccessDB(db_query)
+    for result in db_result:
         print(result[0] + " - " + str(result[1]) + " views")
-
-    db_cursor.close()
-    db.close()
 
 def getPopularAuthors():
 
-    db_query2 = '''
+    db_query = '''
     SELECT authors.name, COUNT(log.status) AS views
     FROM authors JOIN articles ON authors.id = articles.author
     JOIN log ON '/article/' || articles.slug = log.path
@@ -37,19 +40,13 @@ def getPopularAuthors():
     ORDER BY views DESC;
     '''
 
-    db = psy.connect(database = "news")
-    db_cursor = db.cursor()
-    db_cursor.execute(db_query2)
-    query_result = db_cursor.fetchall()
-    for result in query_result:
+    db_result = connectAccessDB(db_query)
+    for result in db_result:
         print(result[0] + " - " + str(result[1]) + " views")
-
-    db_cursor.close()
-    db.close()
 
 def getDaysErrors():
 
-    db_query3 = '''
+    db_query = '''
     SELECT authors.name, COUNT(log.status = '200 OK') AS views
     FROM authors JOIN articles ON authors.id = articles.author
     JOIN log ON '/article/' || articles.slug = log.path
