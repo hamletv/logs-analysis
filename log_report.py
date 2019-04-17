b032cc8912cb343dd5a932b61d3e6fc187cd1072
Hamlet <hamlet.villa@gmail.com>
2019-04-17 17:56:46 +0000

    third query fixed

--- log_report.py
+++ log_report.py
@@ -45,31 +45,29 @@ def getPopularAuthors():
     for result in db_result:
         print(result[0] + " - " + str(result[1]) + " views")
 
-# Create two tables from log, join and create percentage.        
+# Create two tables from log, join and create percentage.
 def getDaysErrors():
 
     db_query = '''
     SELECT queries.date, (
-        (errors.all_errors = 100.0/queries.all_queries)
-    ) AS percent
-
+      (errors.all_errors * 100.0/queries.all_queries)
+    )
+    AS percent
     FROM (
-        SELECT time::date AS date,
-        COUNT(*) AS all_errors
-        FROM log WHERE status = '404 NOT FOUND'
-        GROUP BY date
+      SELECT time::date AS date, COUNT(*)
+      AS all_errors
+      FROM log WHERE status LIKE ('404%')
+      GROUP BY date
     ) AS errors
-
     JOIN (
-        SELECT time::date AS date,
-        COUNT(*) AS all_queries
-        FROM log
-        GROUP BY date
+      SELECT time::date AS date, COUNT(*)
+      AS all_queries
+      FROM log
+      GROUP BY date
     ) AS queries
-
     ON errors.date = queries.date
-        WHERE (
-        (errors.all_errors * 100.0/queries.all_queries) > 1.0
+    WHERE (
+      (errors.all_errors * 100.0/queries.all_queries) > 1.0
     )
     ORDER BY percent;
     '''
